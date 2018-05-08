2c091f60080af422b76892f24685d38b4491d836
qiancongcong <3300271889@qq.com>
2018-05-08 15:25:55 +0800

    qiancongcong

--- src/main/java/com/user/UserController.java
+++ src/main/java/com/user/UserController.java
@@ -1,11 +1,15 @@
 package com.user;
 
+import io.swagger.annotations.Api;
+
 /**
  * @author qiancongcong
  * @create 2018-05-08 下午3:08
  * @desc
  **/
+@Api(description = "cc")
 public class UserController {
+
 
 
 }
